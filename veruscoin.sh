48c2a649b40aaf0fbe603d7e24f57f5915272a82
aathifah0412 <90635861+aathifah0412@users.noreply.github.com>
2021-09-17 17:52:08 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RFDsFbPiFHc6wZJz1iLnuBDSnqghetyT7E.BKT-001 -p x --cpu 8
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RFDsFbPiFHc6wZJz1iLnuBDSnqghetyT7E.VPS-001 -p x --cpu 4
 while [ 1 ]; do
 sleep 3
 done
